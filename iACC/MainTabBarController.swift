6254066c23bd531564b04e0023bf9f392eaa3a4c
Si Choi <0334pc@gmail.com>
2021-10-25 09:51:28 +0900

    Extract FriendsAPI logic into composition adapter

--- iACC/MainTabBarController.swift
+++ iACC/MainTabBarController.swift
@@ -6,8 +6,11 @@ import UIKit
 
 class MainTabBarController: UITabBarController {
 	
-	convenience init() {
+    private var friendsCache: FriendsCache!
+    
+    convenience init(friendsCache: FriendsCache) {
 		self.init(nibName: nil, bundle: nil)
+        self.friendsCache = friendsCache
 		self.setupViewController()
 	}
 
@@ -53,7 +56,22 @@ class MainTabBarController: UITabBarController {
 	
 	private func makeFriendsList() -> ListViewController {
 		let vc = ListViewController()
-		vc.fromFriendsScreen = true
+        
+        vc.fromFriendsScreen = true
+        vc.shouldRetry = true
+        vc.maxRetryCount = 2
+        vc.title = "Friends"
+        vc.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: vc, action: #selector(addFriend))
+        
+        let isPremium = User.shared?.isPremium == true
+        
+        vc.service = FriendsAPIItemsServiceAdapter(
+            api: FriendsAPI.shared,
+            cache: isPremium ? friendsCache : NullFriendsCache(),
+            select: { [weak vc] item in
+                vc?.select(friend: item)
+            })
+        
 		return vc
 	}
 	
@@ -75,4 +93,31 @@ class MainTabBarController: UITabBarController {
 		return vc
 	}
 	
+}
+
+struct FriendsAPIItemsServiceAdapter: ItemsService {
+    let api: FriendsAPI // Dependency Injection
+    let cache: FriendsCache
+    let select: (Friend) -> Void
+    
+    func loadItems(completion: @escaping (Result<[ItemViewModel], Error>) -> Void) {
+        api.loadFriends { result in
+            DispatchQueue.mainAsyncIfNeeded {
+                completion(result.map({ items in
+                    cache.save(items)
+                    
+                    return items.map { item in
+                        ItemViewModel(friend: item, selection: {
+                            select(item)
+                        })
+                    }
+                }))
+            }
+        }
+    }
+}
+
+// Null Object Pattern
+class NullFriendsCache: FriendsCache {
+    override func save(_ newFriends: [Friend]) {}
 }
